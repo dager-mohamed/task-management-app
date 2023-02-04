751b04b571bbe8e4d3f5123545df2739482cf618
MohamedAshraf <mohmed6002090@gmail.com>
2023-02-04 07:19:18 +0200

    deploying to vercel

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,14 +8,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const utils_1 = require("./utils");
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = yield (0, utils_1.createApp)();
-        app.get('/', (req, res) => res.send('ok'));
+        app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
+        app.get("*", (_, res) => {
+            res.sendFile(path_1.default.join(__dirname, "../../frontend/dist/index.html"), (err) => {
+                res.status(500).send(err);
+            });
+        });
         app.listen(process.env.PORT, () => console.log(`[EXPRESS] listening on ${process.env.PORT} port`));
     });
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import {config} from 'dotenv'
 config();
 import { createApp } from './utils'
+import express from 'express'
+import path from 'path'
 
 async function main(){
     const app = await createApp()
-    app.get('/', (req, res) => res.send('ok'))
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"), (err) => {
+            res.status(500).send(err)
+        })
+    })
+
     app.listen(process.env.PORT, () => console.log(`[EXPRESS] listening on ${process.env.PORT} port`))
 }
 main()
